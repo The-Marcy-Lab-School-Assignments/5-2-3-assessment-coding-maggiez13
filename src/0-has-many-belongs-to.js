6e4d22f77bc3be23ea0b417fb466ba286c251cbb
Mei Zhu <maggiez0613@gmail.com>
2024-07-18 15:45:12 -0400

    coding done

--- src/0-has-many-belongs-to.js
+++ src/0-has-many-belongs-to.js
@@ -1,15 +1,52 @@
 const { getId } = require('./utils');
 
 class ToDoItem {
-  constructor() {
+  constructor(description, priorityLevel, isDone = false) {
     this.id = getId();
+    this.description = description; 
+    this.priorityLevel = priorityLevel;
+    this.isDone = isDone; 
+    this.createdAt = new Date();
+  }
+  updateDescription(newDescription) {
+    this.description = newDescription; 
+  }
+  updatePriority(newPriorityLevel) {
+    this.priorityLevel = newPriorityLevel;
+  }
+  markAsCompleted() {
+    this.isDone = true; 
   }
 }
 
-
 class ToDoList {
-  constructor() {
+  static instances = [];
+  constructor(name) {
     this.id = getId();
+    this.name = name; 
+    this.createdAt = new Date();
+    this.items = [];
+    ToDoList.instances.push(this);
+  }
+  createItem(description, priorityLevel) {
+    const newItem = new ToDoItem(description, priorityLevel);
+    this.items.push(newItem);
+    return newItem; 
+  }
+  getItems() {
+    return [...this.items];
+  }
+  getCompletedCount() {
+    return this.items.filter(item => item.isDone).length;
+  }
+  removeItem(idOfItem) {
+    this.items = this.items.filter(item => item.id !== idOfItem);
+  }
+  static list() {
+    return [...ToDoList.instances];
+  }
+  static findBy(id) {
+    return ToDoList.instances.find(list => list.id === id);
   }
 }
 
--- src/1-inheritance-polymorphism.js
+++ src/1-inheritance-polymorphism.js
@@ -1,9 +1,53 @@
 class Phone {
-
+  constructor(number) {
+    this.phoneNumber = number; 
+    this.contacts = [];
+  }
+  addContact(contact) {
+    const { name, phoneNumber } = contact;
+    if (!name || !phoneNumber || phoneNumber.length !== 10) {
+      return "Invalid";
+    }
+    this.contacts.push(contact);
+    return `${name} added.`; 
+  }
+  removeContact(name) {
+    const idx = this.contacts.findIndex(contact => contact.name === name);
+    if (idx === -1) return "Contact not found.";
+    this.contacts.splice(idx, 1);
+    return `${name} removed.`; 
+  }
+  makeCall(contactInfo) {
+    if (contactInfo.length === 10) {
+      const contactSearch = this.contacts.find(contact => contact.phoneNumber === contactInfo);
+      if (contactSearch) {
+        return `Calling ${contactSearch.name}...`;
+      } else {
+        return `Calling ${contactInfo}...`; // still valid phone #, just not in contacts
+      }
+    } else {
+      const contactSearch = this.contacts.find(contact => contact.name === contactInfo);
+      if (contactSearch) {
+        return `Calling ${contactSearch.name}...`;
+      } else {
+        return "Invalid";
+      }
+    }
+  }
 }
 
-class AppleIPhone {
-
+class AppleIPhone extends Phone {
+  constructor(number, model) {
+    super(number);
+    this.model = model; 
+  }
+  sendIMessage(model, message) {
+    if (model instanceof AppleIPhone) {
+      return `Message: ${message} - sent from ${this.model}`;
+    } else {
+      return "Message could not be sent."
+    }
+  }
 }
 
 module.exports = {
